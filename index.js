df045d0fa3494e5ed2d2f0e003d5180ac43d3b3a
p0ysenn <p0ysenn@gmail.com>
2021-06-28 19:47:18 +0200

    Added MongoDB

--- index.js
+++ index.js
@@ -1,31 +1,88 @@
+/*
+Running KarenOS v1.0
+$help for all Commands
+"So, typical day of failure, I see, huh darling?"
+*/
+const config = require('./config.json');
+const { MongoClient } = require('mongodb');
 const Discord = require("discord.js");
+
 const clientBot = new Discord.Client();
-const token = "ODU3MDI0MTM0NjQyNTMyMzYy.YNJj7A.jT9I1oZxbuFnjugTaVbgzlC6Pc4";
-const prefix = "$";
-const adminPing = "<@!280703894269460490>";
-const ugurPing = "<@!246941907287015426>";
-var timeoutPing = "";
+const mongoclient = new MongoClient(config.mongodb.url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+
+
+
+clientBot.login(config.discordJS.token);
 
 clientBot.on("ready", () => {
-    console.log("Bot is online")
+    console.log("Bot is online");
 })
 
-clientBot.login(token);
-
 clientBot.on("message", msg => {
-    if (msg.content.startsWith(prefix) && !msg.author.bot) {
-        let args = msg.content.substring(prefix.length).split(" ");
+    if (msg.content.startsWith(config.discordJS.prefix) && !msg.author.bot) {
+        let args = msg.content.substring(config.discordJS.prefix.length).split(" ");
         switch (args[0]) {
             case "hs":
-                msg.reply("Fick deine Buntstiftdose! " + ugurPing);
+                let ping = "<@!" + ugurID + ">";
+                msg.channel.send("Fick deine Buntstiftdose! " + ping);
                 break;
             case "timeout":
-                //add Args[1] to timeout list
-                //Get date.now() and add args[2]
-                //Wait until end of timeout
+                let time = Date.now();
+                let out = parseInt(args[2]) * 60000;
+                timeoutPings.push({
+                    "id": args[1],
+                    "endtime": time + out
+                });
+                console.log(timeoutPings);
+                msg.reply("Added User to Timeout List until: " + new Date(time + out));
                 break;
             case "debug":
-                msg.reply(adminPing);
+                timeoutUser(args[1], 124);
+                break;
+            case "list":
+                msg.reply("Comming in V1.2")
+                break;
+            case "playlist":
+                msg.reply("Comming in V1.3");
+                break;
+            default:
+                msg.reply("Unknown Command");
+                break;
         }
     }
 });
+
+
+function timeoutUser(id, time) {
+    MongoClient.connect(config.mongodb.url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("KarenBot");
+        var timeoutObj = {
+            _id: id,
+            time
+        };
+        //TODO Check DB for existing ID
+        var query = { _id: id };
+        dbo.collection("timeouts").find(query).toArray(function(err, result) {
+            if (err) throw err;
+            console.log(result);
+            db.close();
+        });
+        if (result.length == 0) {
+            dbo.collection("timeouts").insertOne(timeoutObj, function(err, res) {
+                if (err) throw err;
+                console.log("1 document inserted");
+                db.close();
+            });
+        } else {
+            var newvalues = { $set: { name: "Mickey", address: "Canyon 123" } };
+            dbo.collection("customers").updateOne(query, newvalues, function(err, res) {
+                if (err) throw err;
+                console.log("1 document updated");
+                db.close();
+            });
+        }
+    });
+}
