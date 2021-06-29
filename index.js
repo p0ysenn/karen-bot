c8af29f9c94f9cd91e32d4975ec2ebe21fa8b43e
p0ysenn <p0ysenn@gmail.com>
2021-06-29 15:06:24 +0200

    Implemented Timeout and Delete Funktions

--- index.js
+++ index.js
@@ -6,83 +6,154 @@ $help for all Commands
 const config = require('./config.json');
 const { MongoClient } = require('mongodb');
 const Discord = require("discord.js");
+var idList = [];
 
 const clientBot = new Discord.Client();
-const mongoclient = new MongoClient(config.mongodb.url, { useNewUrlParser: true, useUnifiedTopology: true });
-
-
-
-
 
 clientBot.login(config.discordJS.token);
 
 clientBot.on("ready", () => {
     console.log("Bot is online");
+    MongoClient.connect(config.mongodb.url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db(config.mongodb.database);
+        var query = { bannedAt: { $gt: 1 } };
+        dbo.collection(config.mongodb.collection).find(query).forEach(e => {
+            idList.push(e);
+        });
+    });
 })
 
 clientBot.on("message", msg => {
+    console.log(msg.author.username + ": " + msg.content);
     if (msg.content.startsWith(config.discordJS.prefix) && !msg.author.bot) {
         let args = msg.content.substring(config.discordJS.prefix.length).split(" ");
         switch (args[0]) {
+            case "help":
+                var helpEmbedd = new Discord.MessageEmbed().setColor("FF0101");
+                helpEmbedd.setTitle("Options");
+                helpEmbedd.addFields({ name: "$help", value: "List aller Commands" }, { name: "$hs", value: "Ugur Beleidigen" }, { name: "$timeout + User + Zeit in Sekunden", value: "Hindert User daran für X Sekunden Nachrichten zu schreiben" }, { name: "$list", value: "Liste aller Timeouts" }, { name: "$unban + User", value: "Hebt Timeout auf" }, { name: "$playlist", value: "Spiel Krosse Krabbe 3 Playlist ab" }, { name: "$reminder + Zeit", value: "Errinere Mich in X Zeit an etwas" });
+                msg.channel.send({ embed: helpEmbedd });
+                break;
             case "hs":
-                let ping = "<@!" + ugurID + ">";
+                let ping = "<@!" + config.discordJS.ugurID + ">";
                 msg.channel.send("Fick deine Buntstiftdose! " + ping);
                 break;
             case "timeout":
-                let time = Date.now();
-                let out = parseInt(args[2]) * 60000;
-                timeoutPings.push({
-                    "id": args[1],
-                    "endtime": time + out
+                var bannAuthor = msg.author.id;
+                var bannedAt = Date.now() / 1000;
+                var bannedUntil = bannedAt + parseInt(args[2]);
+                var bannedID = args[1];
+
+
+                timeoutObject = {
+                    _id: bannedID,
+                    bannAuthor,
+                    bannedAt,
+                    bannedUntil,
+                    bannedID
+                }
+
+                MongoClient.connect(config.mongodb.url, function(err, db) {
+                    if (err) throw err;
+                    var dbo = db.db(config.mongodb.database);
+                    var query = { _id: bannedID };
+                    dbo.collection(config.mongodb.collection).findOne(query, function(err, result) {
+                        if (err) throw err;
+                        if (result == undefined || result == null) {
+                            insertUser(timeoutObject);
+                            console.log("No Matching ID found");
+                        } else {
+                            updateUser(timeoutObject);
+                            console.log("Found Matching ID in Database");
+                        }
+                        db.close();
+                    });
                 });
-                console.log(timeoutPings);
-                msg.reply("Added User to Timeout List until: " + new Date(time + out));
-                break;
-            case "debug":
-                timeoutUser(args[1], 124);
+                msg.reply("Added " + bannedID + " to Timeout List until: " + new Date(bannedUntil * 1000));
                 break;
             case "list":
-                msg.reply("Comming in V1.2")
+                var listEmbedd = new Discord.MessageEmbed().setColor("FF0101");
+                listEmbedd.setTitle("Timeouts:");
+                idList.forEach(e => {
+                    listEmbedd.addFields({ name: e.bannedID, value: "Banned until : " + new Date(e.bannedUntil) * 1000 });
+                });
+                msg.channel.send({ embed: listEmbedd });
+                break;
+            case "unban":
+                idList.forEach(e => {
+                    if (e.bannedID == args[1]) {
+                        removeUser(args[1]);
+                        var index = idList.indexOf(args[1]);
+                        idList.pop(index);
+                    }
+                });
                 break;
             case "playlist":
                 msg.reply("Comming in V1.3");
+                break;
+            case "reminder":
+                msg.reply("Comming in V1.3");
+                break;
+            case "debug":
                 break;
             default:
                 msg.reply("Unknown Command");
                 break;
         }
+    } else {
+        idList.forEach(e => {
+            if (e.bannedID == "<@!" + msg.author.id + ">") {
+                if (Date.now() / 1000 >= e.bannedUntil) {
+                    removeUser("<@!" + msg.author.id + ">");
+                    var index = idList.indexOf(e);
+                    idList.pop(index);
+                } else {
+                    deleteHandler(msg);
+                }
+            }
+        })
     }
 });
 
-
-function timeoutUser(id, time) {
+function updateUser(timeoutObj) {
     MongoClient.connect(config.mongodb.url, function(err, db) {
-        if (err) throw err;
-        var dbo = db.db("KarenBot");
-        var timeoutObj = {
-            _id: id,
-            time
-        };
-        //TODO Check DB for existing ID
-        var query = { _id: id };
-        dbo.collection("timeouts").find(query).toArray(function(err, result) {
+        var dbo = db.db(config.mongodb.database);
+        var query = { id: timeoutObj.id };
+        dbo.collection(config.mongodb.collection).updateOne(query, { $set: timeoutObj }, function(err, res) {
             if (err) throw err;
-            console.log(result);
+            console.log("1 document updated");
             db.close();
         });
-        if (result.length == 0) {
-            dbo.collection("timeouts").insertOne(timeoutObj, function(err, res) {
-                if (err) throw err;
-                console.log("1 document inserted");
-                db.close();
-            });
-        } else {
-            var newvalues = { $set: { name: "Mickey", address: "Canyon 123" } };
-            dbo.collection("customers").updateOne(query, newvalues, function(err, res) {
-                if (err) throw err;
-                console.log("1 document updated");
-                db.close();
-            });
-        }
     });
+}
+
+function insertUser(timeoutObj) {
+    MongoClient.connect(config.mongodb.url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db(config.mongodb.database);
+        dbo.collection(config.mongodb.collection).insertOne(timeoutObj, function(err, res) {
+            if (err) throw err;
+            idList.push(timeoutObj);
+            console.log("1 document inserted");
+            db.close();
+        });
+    });
+}
+
+function removeUser(userID) {
+    MongoClient.connect(config.mongodb.url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db(config.mongodb.database);
+        var myquery = { _id: userID };
+        dbo.collection(config.mongodb.collection).deleteOne(myquery, function(err, obj) {
+            if (err) throw err;
+            console.log("1 document deleted");
+            db.close();
+        });
+    });
+}
+
+function deleteHandler(msg) {
+    msg.delete().then(console.log("1 message deleted")).catch(console.error);
 }
